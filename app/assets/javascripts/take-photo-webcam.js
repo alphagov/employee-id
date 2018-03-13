aa54b0fee3793a96868b0fe8fe238491ba0863f0
Stephen McCarthy <stephen.mccarthy@digital.cabinet-office.gov.uk>
2018-03-13 10:43:37 +0000

    added js that enable the 2 taken photos to appear on the photo match page

--- app/assets/javascripts/take-photo-webcam.js
+++ app/assets/javascripts/take-photo-webcam.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   function readURL(input) {
+      let storeAsKey = $(input).data('store-as');
       if (input.files && input.files[0]) {
           var reader = new FileReader();
           reader.onload = function(e) {
               $('#previewImage').attr('src', e.target.result);
+              localStorage[storeAsKey] = e.target.result;
           }
           reader.readAsDataURL(input.files[0]);
       }
@@ -14,4 +16,18 @@ $(document).ready(function(){
       $('#upload-section').show()
       readURL(this);
   });
+
+  $("[data-fetch-from-storage]").each(function() {
+    let $input = $(this);
+    let storedAsKey = $input.data("fetch-from-storage");
+    if (storedAsKey) {
+      $input.attr('src', localStorage[storedAsKey])  
+    }
+  });
+
+  $("[data-clear-local-storage").each(function() {
+    localStorage["webcam"] = undefined;
+    localStorage["document"] = undefined;
+  });
+
 })
